ed67d9d04bda754c499dd3ecb8e2a7694a9d6f61
Joe Pham <rungxanh2901@gmail.com>
2021-01-06 15:43:48 -0500

    #100DaysWithSwift: #Day19.3 Project2
    * #Day19, reading 3: Making the basic game work (UIButton & CALayer)
    
    * Established an array to hold country names & a var to hold initial player’s score
    
    * Set border width of buttons with borderWidth of CALayer (Core Animation data type responsible for managing the way a view looks)
    
    * Set border colour of buttons with cgColor (used to convert a UIColor value to lower level CGColor & then assign that converted value to borderColor of CALayer)
    
    * Created askQuestion() method to generate a random country flag for each button via setImage() method of each UIButton object
    
    * Param "for" of setImage() assigned with ".normal" to specify the standard state of the button
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project2/Project2/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project2/Project2/ViewController.swift
@@ -12,9 +12,38 @@ class ViewController: UIViewController {
   @IBOutlet var button2: UIButton!
   @IBOutlet var button3: UIButton!
   
+  // An array to hold country names
+  var countries = [String]()
+  // A var to hold player's current score
+  var score = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    // Fill up countries array with asset flags
+    countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    
+    // Set borderWith of all buttons
+    button1.layer.borderWidth = 1
+    button2.layer.borderWidth = 1
+    button3.layer.borderWidth = 1
+    
+    // Set borderColor of all buttons
+    // Include .cgColor to convert UIColor values -> CGColor to assign to borderColor of CALayer
+    button1.layer.borderColor = UIColor(red: 0.86, green: 0.56, blue: 0.15, alpha: 1.00).cgColor
+    button2.layer.borderColor = UIColor(red: 0.86, green: 0.56, blue: 0.15, alpha: 1.00).cgColor
+    button3.layer.borderColor = UIColor(red: 0.86, green: 0.56, blue: 0.15, alpha: 1.00).cgColor
+    
+    // Call askQuestion() method
+    askQuestion()
+  }
+  
+  // Method to show 3 random flags in the 3 buttons
+  func askQuestion() {
+    button1.setImage(UIImage(named: countries[Int.random(in: 0..<countries.count)]), for: .normal)
+    button2.setImage(UIImage(named: countries[Int.random(in: 0..<countries.count)]), for: .normal)
+    button3.setImage(UIImage(named: countries[Int.random(in: 0..<countries.count)]), for: .normal)
   }
 
 
