68897520e20b9cbfa8dc58d88a29e064a895a0a1
Joe Pham <rungxanh2901@gmail.com>
2021-01-06 14:45:57 -0500

    #100DaysWithSwift: #Day19.1, #Day19.2
    * #Day19: Project 2
    * Reading 1: Setting up for Project2
    * Reading 2: Designing the layout
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project2/Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Joe Pham on 2021-01-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet var button1: UIButton!
+  @IBOutlet var button2: UIButton!
+  @IBOutlet var button3: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
