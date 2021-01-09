8d8ff102b728636fd3a30f3abd30dc89b5517f5b
Joe Pham <rungxanh2901@gmail.com>
2021-01-09 15:55:25 -0500

    #100DaysWithSwift: #Day24 Project4
    * #Day24, Project 4: Create a simple browser to let user browse websites from a pre-set list
    * Concepts learned:
    1. import WebKit
    2. WKWebView
    3. WKNavigationDelegate protocol & navigationDelegate property
    4. Class can only inherit from another class, but also can conforms to multiple protocols as a comma-separated list following the parent class
    5. URL methods & data type
    6. allowsBackForwardNavigationGestures
    7. UIAlertController properties (title, message, preferredStyle)
    8. didFinish method to update the view controller’s title as title from the webView
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project4/Project4/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project4/Project4/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+  // Class ViewController inherits UIViewController
+  // and conforms to WKNavigationDelegate protocol
+  
+  // Create the web view
+  var webView: WKWebView!
+  // Alter built-in loadView()
+  override func loadView() {
+    // 1. Assign an empty WKWebView instance to webView
+    webView = WKWebView()
+    
+    // 2. Set webView's navigationDelegate to the current view controller
+    webView.navigationDelegate = self
+    // A delegate is 1 thing acting in place of another
+    // This might be why the website takes the entire screen to display
+    // (i.e. the navigation bar is on top of the website's floating navbar at the moment)
+    
+    // 3. Make the current view of loadView() that webView
+    view = webView
+    
+    // This is the initial website displayed by this app
+    let url = URL(string: "https://www.hackingwithswift.com")!
+    // 1. This assigns a URL object out of String to url
+    webView.load(URLRequest(url: url))
+    // 2. This create a URLRequest object from the url, then feeds webView to load
+    webView.allowsBackForwardNavigationGestures = true
+    // 3. This enables backward/forward edge-swiping gesture
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    // Add a navigation button to hold list of websites (as alert options) to browse
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+
+    }
+  
+  // Obj-C openTapped() method to display UIAlertController options
+  // These options are pre-set websites for users to browse
+  @objc func openTapped() {
+    let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+    ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+    present(ac, animated: true)
+    }
+  
+  // openPage method that takes param value from UIAlertAction above
+  // Then string interpolated it in a complete URL and passed to webView to load as a URLRequest
+  func openPage(action: UIAlertAction) {
+    let url = URL(string: "https://" + action.title!)!
+    webView.load(URLRequest(url: url))
   }
-
-
+  
+  // Update title of webView to title of webpage after finished loading the site
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    title = webView.title
+  }
 }
 
