e2d0c66b08fcbf1f18e5ca61a5e966757eabb585
Joe Pham <rungxanh2901@gmail.com>
2021-01-12 23:41:01 -0500

    #100DaysWithSwift: #Day27 Project5 Setting up
    * Purpose of the app: Create a word game that deals with anagrams
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Joe Pham on 2021-01-12.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
