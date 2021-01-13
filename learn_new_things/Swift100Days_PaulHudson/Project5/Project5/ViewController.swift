5d7d8af8783f6f81282a899f2c62b3c10b999cf9
Joe Pham <rungxanh2901@gmail.com>
2021-01-13 09:20:35 -0500

    #100DaysWithSwift: #Day27.3 Project5
    * #Day27: Reading from disk with contentsOf
    
    1. Add an input file containing all words
    2. Declare 2 vars to hold all words, and used words played
    3. Load contents of input file at a bundle URL where it contains the input file name & extension
    4. Break down the string content into separate words at "\n"
    5. Define a fallback case to all words if input content is not loaded correctly
    6. Define main game method: choose a random word, remove all previously used words, force table view to reload data
    7. Define how many rows to be displayed (number of used words played)
    8. Define how each row should look like (name of each used word played)
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project5/Project5/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project5/Project5/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Project5
+//  Project5: Anagram Word Game
 //
 //  Created by Joe Pham on 2021-01-12.
 //
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    // 1st array to hold all game words from input file "start.txt"
+    var allWords = [String]()
+    // 2nd array to hold all words the player has used in this game
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Find URL path to "start.txt" file in the filesystem
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // Create a string from all text contents of the input file
+            // Use "try?" to show if the code throws an error, return nil instead
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // Break down this string into words at each "\n"
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        // But even if "try?" returns nil, we have a backup here for allWords
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        // Start the game
+        startGame()
     }
 
-
+    // Main game method
+    func startGame() {
+        // Show title as 1 random word in the 1st array
+        // Also the word the player has to find
+        title = allWords.randomElement()
+        
+        usedWords.removeAll(keepingCapacity: true)
+        
+        // Call table view's reloadData() to force call numberOfRowsInSection again
+        // Also it calls cellForRowAt repeatedly -> all to check if data loaded correctly
+        tableView.reloadData()
+    }
+    
+    // Foundation methods to handle table view data
+    // 1. numberOfRowsInSection
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    // 2. cellForRowAt
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
