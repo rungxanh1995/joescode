4f1103758baeb77566a8ae98212846fb29d0c764
Joe Pham <rungxanh2901@gmail.com>
2021-01-16 22:48:06 -0500

    #100DaysWithSwift: #Day31.2 Auto Layout anchors
    * #Day31, reading 2: Auto Layout anchors (width anchor, height anchor, top anchor, bottom anchor, leading anchor,  trailing anchor, etc.)
    
    * Also added a viewLabels array for iteration
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project6b/Project6b/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project6b/Project6b/ViewController.swift
@@ -42,21 +42,46 @@ class ViewController: UIViewController {
         label5.text = "LABELS"
         label5.sizeToFit()
         
-        view.addSubview(label1)
-        view.addSubview(label2)
-        view.addSubview(label3)
-        view.addSubview(label4)
-        view.addSubview(label5)
+        let viewLabels = [label1, label2, label3, label4, label5]
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-        
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        for label in viewLabels {
+            view.addSubview(label)
         }
-        let metrics = ["labelHeight": 96]
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//        view.addSubview(label1)
+//        view.addSubview(label2)
+//        view.addSubview(label3)
+//        view.addSubview(label4)
+//        view.addSubview(label5)
+
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        let metrics = ["labelHeight": 96]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
        
+        // Auto Layout anchors
+        var previous: UILabel?
+        
+        for label in viewLabels {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                // previous is not nil, we have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // set the top constraint for the first label
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            // Set the previous label to the current one, for the next loop iteration
+            previous = label
+        }
+        
+        
 
     }
 
