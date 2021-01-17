d0dd4f7a96d6cbd2d4cf2a351db0cb7c262a4907
Joe Pham <rungxanh2901@gmail.com>
2021-01-17 11:07:22 -0500

    #100DaysWithSwift: #Day32 Project4-6 1st push
    * Create an app that lets people create a shopping list by adding items to a table view
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project4-6/Project4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4-6
+//
+//  Created by Joe Pham on 2021-01-17.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
