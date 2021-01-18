4ead464024bfa15e125adbffd6fe8b2cdde5fe8a
Joe Pham <rungxanh2901@gmail.com>
2021-01-17 22:46:34 -0500

    #100DaysWithSwift: #Day33 Project7 Setting Up
    * A voting app
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project7/Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Joe Pham on 2021-01-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ learn_new_things/javaScript/script.js
@@ -0,0 +1,4 @@
+const functionName = (params) => {
+    return `Hello ${params}`;
+}
+console.log(functionName("hello"));
