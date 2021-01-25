c40a209a2dd522756909eba51124778654854365
Joe Pham <rungxanh2901@gmail.com>
2021-01-24 22:02:20 -0500

    #100DaysWithSwift: #Day37.1 Project8
    * #Day37, reading 1: Loading a level and adding button targets
    
    * Added input files with 7 lines of input each, for total of 7 words to guess in a game
    
    * Added a main loadLevel() method to break inputs into lines, the split each line into the answer bit & clue bit.
    
    Each type of bits is then cleaned off of unwanted characters, then stiched together with a "\n" newline character.
    
    Each stiched string is then cleaned off it traliing whitespaces and newline chars, and then used to replace the initial text value of: clues label & answers label.
    
    Furthermore, the enumeration method was used to add ordinal numbers to each clue line.
    
    Title of each letter button is also replaced with the value of each shuffled array of letter buttons and letter bits.
    
    * Added targets to the submit, clear, and letter buttons.
    
    Also laid out barebones for the methods to be called upon tapping each of these buttons.
    
    * Added the original player score value, and level value of each game
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project8/Project8/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project8/Project8/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Laying out UI vars
     var cluesLabel: UILabel!
     var answersLabel: UILabel!
     var currentAnswer: UITextField!
     var scoreLabel: UILabel!
     var letterButtons = [UIButton]()
+    
+    // Store the buttons being used to spell an answer
+    var activatedButtons = [UIButton]()
+    
+    // Store possible answers
+    var possibleAnswers = [String]()
+    
+    var score = 0   // player score
+    var level = 1   // current game lavel
     
     override func loadView() {
         // Create initial, top root view
@@ -61,12 +70,14 @@ class ViewController: UIViewController {
         let submitButton = UIButton(type: .system)
         submitButton.translatesAutoresizingMaskIntoConstraints = false
         submitButton.setTitle("SUBMIT", for: .normal)
+        submitButton.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         view.addSubview(submitButton)
         
         // Create a Clear button
         let clearButton = UIButton(type: .system)
         clearButton.translatesAutoresizingMaskIntoConstraints = false
         clearButton.setTitle("CLEAR", for: .normal)
+        clearButton.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         view.addSubview(clearButton)
         
         // Create a UIView to host the word buttons
@@ -149,6 +160,9 @@ class ViewController: UIViewController {
                 // Give each button some default text for testing
                 letterButton.setTitle("ABC", for: .normal)
                 
+                // Add target to each button
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+                
                 // Calculate the button frame using its column & row
                 let buttonFrame = CGRect(x: column * buttonWidth, y: row * buttonHeight, width: buttonWidth, height: buttonHeight)
                 letterButton.frame = buttonFrame
@@ -165,9 +179,89 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Call the main loading game method
+        loadLevel()
     }
 
-
+    // Method when each letter button is tapped
+    @objc func letterTapped(_ sender: UIButton) {
+        
+    }
+    
+    // Method when Submit button is tapped
+    @objc func submitTapped(_ sender: UIButton) {
+        
+    }
+    
+    // Method when Clear button is tapped
+    @objc func clearTapped(_ sender: UIButton) {
+        
+    }
+    
+    // Main method to load a game level
+    func loadLevel() {
+        // Store all clues of the level
+        var clueString = ""
+        // Store the hint of how many letters each answer has
+        var cleanedAnswerString = ""
+        // Store all the letter groups, eg. "HA,UNT, ED"
+        var letterBits = [String]()
+        
+        // Search the app bundle for level text file
+        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            // Load the text content of the level file
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                // Break down the content into array of lines at the newline character
+                var lines = levelContents.components(separatedBy: "\n")
+                
+                // Shake up the lines array so order of these lines are different each time the game is played
+                lines.shuffle()
+                
+                // Loop through the array of lines using an "index" and "line"
+                for (index, line) in lines.enumerated() {
+                    // Break down a line into parts at ": "
+                    let parts = line.components(separatedBy: ": ")
+                    // Raw answer is at index 0 of each part
+                    let rawAnswer = parts[0]
+                    // The clue is at index 1 of each part
+                    let clue = parts[1]
+                    
+                    // Define the string for a clue then add to the clue string
+                    clueString += "\(index + 1).\t\(clue)\n"
+                    
+                    // Now clean the "|" off of eacg raw answer to add them to the solution string
+                    let cleanedAnswerWord = rawAnswer.replacingOccurrences(of: "|", with: "")
+                    // Define how the hint of number of letters is displayed
+                    cleanedAnswerString += "\(cleanedAnswerWord.count) letters\n"
+                    // Add the solution word to possibleAnswers array
+                    possibleAnswers.append(cleanedAnswerWord)
+                    
+                    // Also, break down the original raw answer with "|"s in between into array of 3 elements
+                    let bits = rawAnswer.components(separatedBy: "|")
+                    // Then add these bits into the letter bits array of each answer
+                    letterBits += bits
+                }
+            }
+        }
+        
+        // Now change initial texts of cluesLabel and answersLabel to cleaned clues and hints via loadLevel()
+        // Trim the last "\n" off of each string
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = cleanedAnswerString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Though the answer words were shuffled, their letter bits aren't
+        // Which means the letter buttons flow does give off all the answers
+        // Hence shuffling the letter bits array too
+        letterBits.shuffle()
+        
+        // Ensure no. of letter buttons on screen is same with no. of letter bits in input file's raw answers
+        if letterButtons.count == letterBits.count {
+            // Then use a for loop to assign each letter button's title value
+            for i in 0..<letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
 }
 
