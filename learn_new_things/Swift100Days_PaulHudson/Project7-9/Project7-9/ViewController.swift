8854e3f740a5476bcb9973ff6776ef4fcd4037f7
Joe Pham <rungxanh2901@gmail.com>
2021-02-01 00:28:37 -0500

    #100DaysWithSwift: #Day41 Project7-9 Barebones UI
    * #Day41, milestone Project7-9: Create a hangman game
    * Created the barebones UI for the game
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project7-9/Project7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-9
+//
+//  Created by Joe Pham on 2021-01-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
