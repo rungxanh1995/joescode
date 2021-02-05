4cdbecf9cce884cdfab17d6d4a33173815a1bc0d
Joe Pham <rungxanh2901@gmail.com>
2021-02-05 00:18:07 -0500

    #100DaysWithSwift: #Day42.3 Project10
    * #Day42, reading 3: UICollectionView data sources
    
    * Learned about the so-called 'equivalent' to numberOfLinesInSection() method of UICollectionView
    
    * Introduced to the fatalError() method when a collection view cell can’t be typecasted as a custom pre-defined collection view cell
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
@@ -13,7 +13,21 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO: Temp use of 10 items, to switch to using an array soon
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            // Failed to get a PersonCell
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        
+        // Otherwise if got a PersonCell, return it
+        return cell
+    }
 
 }
 
