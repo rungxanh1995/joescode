cb1aa8ed8d23f9d347ebe9a762a1f6ca443ef9b4
Joe Pham <rungxanh2901@gmail.com>
2021-02-05 18:39:30 -0500

    #100DaysWithSwift: #Day43.1 Project10
    * #Day43, reading 1: Importing photos with UIImagePickerController
    
    * To pull up the UIImagePickerController, you gotta delegate to self. And to do so, you gotta conform the ViewController to UIImagePickerControllerDelegate, and UINavigationControllerDelegate
    
    * Alter the didFinishPickingMediaWithInfo() method to write the image data to disk using its UUID string name, appended to the path of the app’s Documents folder, and written to disk via the write() method
    
    * Custom define a method to get the path to the  Documents directory of the app
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    // Custom method to get the Documents directory assigned to the app
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
