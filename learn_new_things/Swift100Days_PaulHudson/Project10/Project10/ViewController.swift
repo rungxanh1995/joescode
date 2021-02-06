9895dac399fa677b137cc2b39978157be663f08d
Joe Pham <rungxanh2901@gmail.com>
2021-02-05 23:41:43 -0500

    #100DaysWithSwift: #Day43.2 Project10
    * #Day43, reading 2: Custom subclasses of NSObject
    
    * Created a new Cocoa Touch class named Person.swift to hold a Person class, with a string name and string image.
    
    * Added an empty Person array named 'people' to ViewController.swift
    
    * Started to create a new Person object each time the image is selected from the UIImageControllerPicker
    
    * Changed the number of items in section to count of the people array
    
    * A few small changes in Storyboard to display the collection view cell better in both light and dark mode
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project10/Project10/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +34,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             try? jpegData.write(to: imagePath)
         }
         
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
         dismiss(animated: true)
     }
     
@@ -43,7 +49,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO: Temp use of 10 items, to switch to using an array soon
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
