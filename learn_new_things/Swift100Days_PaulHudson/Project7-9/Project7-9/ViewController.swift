afc413551338fac1584203e41c2c77ea63948993
Joe Pham <rungxanh2901@gmail.com>
2021-02-10 18:12:05 -0500

    #100DaysWithSwift: #Day41 Project7-9 [UNFINISHED]
    * #Day41, Consolidation Project6-9: Build a hangman game
    
    * Got the UI barebones established.
    * Got the conceptual logic almost done
    * Not able to proceed further as of yet
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project7-9/Project7-9/ViewController.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project7-9/Project7-9/ViewController.swift
@@ -134,7 +134,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
     @objc func letterTapped() {
