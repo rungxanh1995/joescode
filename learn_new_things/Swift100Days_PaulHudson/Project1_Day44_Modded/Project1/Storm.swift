71802ce0b73cb3c89f15656aa582889ef557cd28
Joe Pham <rungxanh2901@gmail.com>
2021-02-10 18:09:53 -0500

    #100DaysWithSwift: #Day44.3 Challenge 3 (UNFINISHED)
    * #Day44, Challenge 3: Modify project 1 so that it uses a collection view controller rather than a table view controller.
    
    * Got the UI built, but not able to figure out the underlying work of plucking out local saved files from the app bundle and place them into the UICollectionViewCell as of yet.
    
    * At least no syntax errors for now 😭
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project1_Day44_Modded/Project1/Storm.swift
@@ -0,0 +1,18 @@
+//
+//  Storm.swift
+//  Project1
+//
+//  Created by Joe Pham on 2021-02-08.
+//
+
+import UIKit
+
+class Storm: NSObject {
+    var name: String
+    var image: UIImage
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ learn_new_things/Swift100Days_PaulHudson/Project1_Day44_Modded/Project1/StormCell.swift
@@ -0,0 +1,14 @@
+//
+//  StormCell.swift
+//  Project1
+//
+//  Created by Joe Pham on 2021-02-08.
+//
+
+import UIKit
+
+class StormCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
