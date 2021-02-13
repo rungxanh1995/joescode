f6b5fe382e72bda4142ea3e48ec76c8b6e28c168
Joe Pham <rungxanh2901@gmail.com>
2021-02-13 11:06:03 -0500

    100DaysWithSwift: Day45.2 Project11
    * Day 45, reading 2: Falling boxes: SKSpriteNode, UITouch, SKPhysicsBody
    
    * Define the background (a node), then also add a physics body to it, bounded by the line of each edge (frame)
    
    * Define how a touch on the screen would be, also add a physics body to it of same size, where it would appear first with the touch
    
    * Added the assets for the app in Assets Catalog
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
@@ -12,9 +12,27 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        // DEFINE THE GAME BACKGROUND
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)   // half the scene size
+        background.blendMode = .replace // ignore any alpha values
+        background.zPosition = -1   // place the background below everything else
+        addChild(background)    // background is a node
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)    // add a physics body to the whole scene that's a line on each edge
+        // acting like a container for the scene
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        // DEFINE A TOUCH ON THE SCREEN
+        if let touch = touches.first    // pull out any screen touches from `touches` set
+        {
+            let location = touch.location(in: self) // find out where the screen was touched in relation to self/the game scene
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64)) // add a physics body to the box of same size
+            box.position = location // place the box where the touch happened
+            addChild(box)   // box is also a node, add it to the scene
+        }
     }
 }
