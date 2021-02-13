fb390e048a570d84f7e37d4fa1b2cdd877051412
Joe Pham <rungxanh2901@gmail.com>
2021-02-13 10:23:45 -0500

    100DaysWithSwift: Day45.1 Setting up
    * Day 45, reading 1: Setting up for a Panchiko/Peggle game
    * Note: this is the brushed off, clean-state version of a Game project using SpriteKit
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Joe Pham on 2021-02-13.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
