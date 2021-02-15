9704ecd4099adccbe3190300f86503e1b0e13a3d
Joe Pham <rungxanh2901@gmail.com>
2021-02-15 12:50:48 -0500

    100DaysWithSwift: Day46.2 Project11
    * Day 46, reading 2: Collision detection: SKPhysicsContactDelegate
    
    * Conformed the class `GameScene` to `SKPhysicsContactDelegate`
    
    * Also assign a physics world contact delegate to the current scene. So that we can tell about contact/collision of its child elements
    
    * Assign a collision bitmask to each ball’s contact test bitmask to tell about every collision that happens to the ball
    
    * Having done so, moved on to define how a collision from the ball to the good/bad slots should remove the ball from the scene.
    
    *** We ignore the case where 2 balls bump into each other, because you don’t want to remove either balls when this kind of collision happens (doing this would mean we have to force unwrap a ball node like in the commented code at line 118 & 120). Guard let is there to help
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
@@ -8,9 +8,14 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
+        // ADD A PHYSICS BODY TO THE SCENE
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)    // a line on each edge
+        // acting like a container for the scene
+        
+        physicsWorld.contactDelegate = self // assign the current scene to physics world's contact delegate
         
         // DEFINE THE GAME BACKGROUND
         let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -19,8 +24,11 @@ class GameScene: SKScene {
         background.zPosition = -1   // place the background below everything else
         addChild(background)    // background is a node
         
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)    // add a physics body to the whole scene that's a line on each edge
-        // acting like a container for the scene
+        // CREATE THE SLOTS (SO IT'S BEHIND THE BOUNCERS)
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
         
         // CREATE THE BOUNCERS
         makeBouncer(at: CGPoint(x: 0, y: 0))
@@ -28,12 +36,6 @@ class GameScene: SKScene {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
-        
-        // CREATE THE SLOTBASES
-        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
-        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
-        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
-        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
     }
     
     // DEFINE THE BOUNCER(s)
@@ -53,17 +55,21 @@ class GameScene: SKScene {
         if isGood {
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"  // name this node the good slot
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"   // name this node the bad slot
         }
         
         slotBase.position = position
         slotGlow.position = position
-//        slotGlow.zPosition = 0
+        
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)    // add rectangle physics
+        slotBase.physicsBody?.isDynamic = false // keep it still
         
         // Make the glow spins, forever
-        let spin = SKAction.rotate(byAngle: (-CGFloat.pi), duration: 5)
+        let spin = SKAction.rotate(byAngle: (-CGFloat.pi), duration: 10)
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
         
@@ -78,10 +84,42 @@ class GameScene: SKScene {
         {
             let location = touch.location(in: self) // find out where the screen was touched in relation to self/the game scene
             let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.name = "ball"  // name this node
             ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0) // add a circular physics body to the ball with radius of half the image width
+            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask   // let it tell about every collision that happens to the ball
             ball.physicsBody?.restitution = 0.4 // change its bounciness within 0-1 range
             ball.position = location // place the ball where the touch happened
             addChild(ball)   // ball is also a node, add it to the scene
+        }
+    }
+    
+    // DEFINE A COLLISION BETWEEN THE BALL AND OTHER OBJECTS WOULD WORK
+    func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball) // call the destroy method defined below
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+    
+    // DEFINE HOW THE BALL IS REMOVED FROM THE NODE TREE
+    func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        // ignore the case if both bodies are balls, we don't want them to disappear when they're both balls
+        
+        // have a guard to prevent force unwrapping an already removed ball node
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA.name == "ball" {
+//            collisionBetween(ball: contact.bodyA.node!, object: contact.bodyB.node!)
+            collisionBetween(ball: nodeA, object: nodeB)
+        } else if nodeB.name == "ball" {
+//            collisionBetween(ball: contact.bodyB.node!, object: contact.bodyA.node!)
+            collisionBetween(ball: nodeB, object: nodeA)
         }
     }
 }
