11022874b613f43aadbf25d76c3ca31fdd09d64f
Joe Pham <rungxanh2901@gmail.com>
2021-02-15 15:50:40 -0500

    100DaysWithSwift: Day46.3 Project11
    * Day 46, reading 3: Scores on the board: SKLabelNode
    
    * Added a score label & editing label
    
    * Editing label goes together with editingMode boolean, that when toggled would allow creating boxes or balls at the touch position, accordingly
    
    * Expanded the codestruct with some extra newline characters
    
    * Deleted 2 lines of commented code in didBegin(_ contact:)
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
+++ learn_new_things/Swift100Days_PaulHudson/Project11/Project11/GameScene.swift
@@ -10,6 +10,25 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    var scoreLabel: SKLabelNode!    // force unwrapped, this score label to be defined in didMove()
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var editingLabel: SKLabelNode!  // allow players to edit the game scene
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editingLabel.text = "Done"  // label text hints to "Done" while in editing mode
+            } else {
+                editingLabel.text = "Edit"  // label text hints to "Edit" while NOT in editing mode
+            }
+        }
+    }
+    
+    
     override func didMove(to view: SKView) {
         // ADD A PHYSICS BODY TO THE SCENE
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)    // a line on each edge
@@ -36,7 +55,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
+        
+        // DEFINE THE SCORE LABEL
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"    // initial value
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
+        // DEFINE THE EDITING LABEL
+        editingLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editingLabel.text = "Edit"
+        editingLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editingLabel)
     }
+    
     
     // DEFINE THE BOUNCER(s)
     func makeBouncer(at position: CGPoint) {
@@ -46,6 +79,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bouncer.physicsBody?.isDynamic = false  // being false means it's gonna stay in place, not moving
         addChild(bouncer)
     }
+    
     
     // DEFINE THE SLOT(s)
     func makeSlot(at position: CGPoint, isGood: Bool) {
@@ -77,37 +111,66 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(slotGlow)
     }
     
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         // DEFINE A TOUCH ON THE SCREEN
         if let touch = touches.first    // pull out any screen touches from `touches` set
         {
             let location = touch.location(in: self) // find out where the screen was touched in relation to self/the game scene
-            let ball = SKSpriteNode(imageNamed: "ballRed")
-            ball.name = "ball"  // name this node
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0) // add a circular physics body to the ball with radius of half the image width
-            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask   // let it tell about every collision that happens to the ball
-            ball.physicsBody?.restitution = 0.4 // change its bounciness within 0-1 range
-            ball.position = location // place the ball where the touch happened
-            addChild(ball)   // ball is also a node, add it to the scene
+            
+            let objects = nodes(at: location)   // get a list of objects at a specific location
+            if objects.contains(editingLabel) {
+                // if that location has the editing label
+                editingMode.toggle()    // toggle means change to true if currently false, and vice versa
+            } else {
+                if editingMode {
+                    // create a box
+                    let size = CGSize(width: Int.random(in: 32...128), height: 16)
+                    let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                    box.zRotation = CGFloat.random(in: 0...3)   // let each box rotates randomly
+                    box.position = location // create the box at that touch point
+                    
+                    box.physicsBody = SKPhysicsBody(rectangleOf: size)  // add a physics body to the box
+                    box.physicsBody?.isDynamic = false
+                    
+                    addChild(box)
+                } else {
+                    // create the balls
+                    let ball = SKSpriteNode(imageNamed: "ballRed")
+                    ball.name = "ball"  // name this node
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0) // add a circular physics body to the ball with radius of half the image width
+                    ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask   // let it tell about every collision that happens to the ball
+                    ball.physicsBody?.restitution = 0.4 // change its bounciness within 0-1 range
+                    ball.position = location // place the ball where the touch happened
+                    ball.position.y = CGFloat(768)  // let the ball fall from top of the screen
+                    addChild(ball)   // ball is also a node, add it to the scene
+                }
+            }
         }
     }
+    
     
     // DEFINE A COLLISION BETWEEN THE BALL AND OTHER OBJECTS WOULD WORK
     func collisionBetween(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball) // call the destroy method defined below
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
+    
     
     // DEFINE HOW THE BALL IS REMOVED FROM THE NODE TREE
     func destroy(ball: SKNode) {
         ball.removeFromParent()
     }
     
+    
     func didBegin(_ contact: SKPhysicsContact) {
+        // WHEN PHYSICS CONTACT HAPPENS
         // ignore the case if both bodies are balls, we don't want them to disappear when they're both balls
         
         // have a guard to prevent force unwrapping an already removed ball node
@@ -115,10 +178,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let nodeB = contact.bodyB.node else { return }
         
         if nodeA.name == "ball" {
-//            collisionBetween(ball: contact.bodyA.node!, object: contact.bodyB.node!)
             collisionBetween(ball: nodeA, object: nodeB)
         } else if nodeB.name == "ball" {
-//            collisionBetween(ball: contact.bodyB.node!, object: contact.bodyA.node!)
             collisionBetween(ball: nodeB, object: nodeA)
         }
     }
