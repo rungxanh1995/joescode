82bbdcb980d441f78d73de3169dce77715da5207
Joe Pham <rungxanh2901@gmail.com>
2021-04-30 16:56:31 -0400

    Today App: Get Started
    1. Getting Started with Today
    2. Creating a Storyboard App
    3. Setting Constraints with Auto Layout
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ SwiftPetProjects/Today/Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Joe Pham on 2021-04-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
