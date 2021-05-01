b8ca8422d97de7716caaa2f939646b3081b21c2c
Joe Pham <rungxanh2901@gmail.com>
2021-05-01 08:10:03 -0400

    Today App: Customize Data Sources
    Now that I have custom cells for editing, I’ll create a new data source to present those cells.
    
    Update the table view to support editing the reminder.
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

---
+++ SwiftPetProjects/Today/Today/Model/Data Sources/ReminderDetailEditDataSource.swift
@@ -0,0 +1,103 @@
+//
+//  ReminderDetailEditDataSource.swift
+//  Today
+//
+//  Created by Joe Pham on 2021-05-01.
+//
+
+import UIKit
+
+class ReminderDetailEditDataSource: NSObject {
+	enum ReminderSection: Int, CaseIterable {
+		case title
+		case dueDate
+		case notes
+		
+		var displayText: String {
+			switch self {
+				case .title:
+					return "Title"
+				case .dueDate:
+					return "Date"
+				case .notes:
+					return "Notes"
+			}
+		}
+		
+		var numRows: Int {
+			switch self {
+				case .title, .notes: return 1
+				case .dueDate: return 2
+			}
+		}
+		
+		func cellIdentifier(for row: Int) -> String {
+			switch self {
+				case .title:
+					return "EditTitleCell"
+				case .dueDate:
+					return row == 0 ? "EditDateLabelCell" : "EditDateCell"
+				case .notes:
+					return "EditNotesCell"
+			}
+		}
+	}
+	
+	static var dateLabelCellIdentifier: String {
+		return ReminderSection.dueDate.cellIdentifier(for: 0)
+	}
+	
+	var reminder: Reminder
+	
+	init(reminder: Reminder) {
+		self.reminder = reminder
+	}
+	
+	private func dequeueAndConfigure(for indexPath: IndexPath, from tableView: UITableView) -> UITableViewCell {
+		guard let section = ReminderSection(rawValue: indexPath.section) else {
+			fatalError("Section index out of range")
+		}
+		let identifier = section.cellIdentifier(for: indexPath.row)
+		let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+		switch section {
+			case .title:
+				if let titleCell = cell as? EditTitleCell {
+					titleCell.configure(title: reminder.title)
+				}
+			case .dueDate:
+				if indexPath.row == 0 {
+					cell.textLabel?.text = reminder.dueDate.description
+				} else {
+					if let dateCell = cell as? EditDateCell {
+						dateCell.configure(date: reminder.dueDate)
+					}
+				}
+			case .notes:
+				if let notesCell = cell as? EditNotesCell {
+					notesCell.configure(notes: reminder.notes)
+				}
+		}
+		return cell
+	}
+}
+
+extension ReminderDetailEditDataSource: UITableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return ReminderSection.allCases.count
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return ReminderSection(rawValue: section)?.numRows ?? 0
+	}
+	
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		guard let section = ReminderSection(rawValue: section) else {
+			fatalError("Section index out of range")
+		}
+		return section.displayText
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return dequeueAndConfigure(for: indexPath, from: tableView)
+	}
+}
